61105f3ab3fc681216e91121ef09b0e978cc89b3
skittskitt <louise.kristin11@gmail.com>
2026-02-09 15:10:49 +0100

    arbeidskrav

---
+++ arbeidskrav2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './style/layout.scss'
+import ShoppingList from './components/ShoppingList'
+
+function App() {
+
+  return (
+    <main>
+      <h1>Handleliste</h1>
+      <ShoppingList />
+    </main>
+  )
+}
+
+export default App
