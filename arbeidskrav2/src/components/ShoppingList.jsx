ddb725964c0bb97b376dcbddcfa4f9d1679a77f2
skittskitt <louise.kristin11@gmail.com>
2026-02-11 11:15:28 +0100

    .

--- arbeidskrav2/src/components/ShoppingList.jsx
+++
@@ -1,9 +0,0 @@
-import ShoppingItem from "./ShoppingItem"
-
-export default function ShoppingList(shoppingListItem){
-    return (
-        <section>
-            {shoppingListItem.map((item) => <ShoppingItem key={item.id} title={title.id} id={item.id} />)}
-        </section>
-    )
-}
