a38990920dafe77161ac5784704c096604925058
skittskitt <louise.kristin11@gmail.com>
2026-03-12 07:11:47 +0100

    .

--- uin26ak3_moviesearch_ostensen/src/components/Preview.jsx
+++ uin26ak3_moviesearch_ostensen/src/components/Preview.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Movies() {
+export default function Preview() {
     const [movies, setMovies] = useState([])
 
     const getMovies = async ()=> {
--- uin26ak3_moviesearch_ostensen/src/pages/Home.jsx
+++ uin26ak3_moviesearch_ostensen/src/pages/Home.jsx
@@ -1,20 +1,21 @@
+import '../assets/style/style.scss'
 import { useEffect, useState } from "react"
+import History from "../components/History"
 
 export default function Home() {
     const [search, setSearch] = useState()
     const storedHistory = localStorage.getItem("search")
+    const [focused, setFocused] = useState(false) 
 
     const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
+    console.log("Storage:", storedHistory) //Viser riktig.
 
+    //API url og nøkkel.
     const apiUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
     const apiKey = import.meta.env.VITE_OMDB_API_KEY
 
-    console.log(apiKey)
-
-    useEffect(()=>{
-        
-        localStorage.setItem("search", JSON.stringify(history))
-    },[history])
+    useEffect(()=>
+        { localStorage.setItem("search", JSON.stringify(history))},[history])
     
     const getMovies = async()=>{
         try
@@ -22,15 +23,15 @@ export default function Home() {
             const response = await fetch(`${apiUrl}${apiKey}`)
             const data = await response.json()
         }
-        catch(err){
-            console.error(err);
-        }
+        catch(err){ console.error(err);}
     }
 
+    //Henter verdien fra søket.
     const handleChange = (e)=>{
         setSearch(e.target.value)
     }
 
+    //Lagrer verdiene i history
     const handleSubmit = (e)=>{
         e.preventDefault()
         e.target.reset()
@@ -39,14 +40,15 @@ export default function Home() {
     }
 
     return (
-        <>
+        <> {/*Kan jeg flytte handleChange fra onChange til en funksjon som endres etter 3 tegn er skrevet inn?*/}
         <main>
             <h1>Forside</h1>
-            <form onSubmit={handleSubmit}>
+            <form className="input-layout" onSubmit={handleSubmit}>
                 <label>
                 Søk etter film
-                    <input type="search" placeholder="" onChange={handleChange} ></input>
+                    <input type="search" placeholder="Princess Mononoke" onChange={handleChange} onFocus={()=> setFocused(true)}></input>
                 </label>
+                {focused ? <History history={history} setSearch={setSearch} /> : null }
                 <button onClick={getMovies}>Søk</button>
             </form>
         </main>
